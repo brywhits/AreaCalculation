e72a798f85ac521ae9666ebb9d8794f731df4b59
bryantwhitsett <bryantwhitsett71@gmail.com>
2025-06-21 17:12:23 -0400

    Created a method to calculate the area of a circle given the user radius entry. If there was an invalid entry, a prompt appears to encourage re-entry of a valid raidis.

---
+++ Area Calculation/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Area_Calculation;
+
+class Program
+{
+    public static double CalculateCircleArea(double radius)
+    {
+        return Math.PI * radius * radius;
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the radius of the circle: ");
+        string input = Console.ReadLine();
+
+        double radius;
+        if (double.TryParse(input, out radius) && radius >= 0)
+        {
+            double area = CalculateCircleArea(radius);
+            
+            Console.WriteLine($"The area of the circle is: {area:F2}");
+        }
+        else
+        {
+            Console.WriteLine("Please enter a non-negative or valid number for the radius. ");
+        }
+    }
+}
